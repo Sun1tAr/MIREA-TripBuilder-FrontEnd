97fadb74e0e638eab02daa4069b61fae586e9e76
Denis Tuev <tuev_dd@nectech.pro>
2025-12-08 14:14:35 +0300

    deploy to remote host 4

--- trip-builder/src/App.jsx
+++ trip-builder/src/App.jsx
@@ -29,6 +29,8 @@ function App() {
               <Routes>
                 <Route path="" element={<Layout />}>
                   <Route index element={<Home />} />
+                  <Route path="home" element={<Home />} />
+
                   <Route path="my-trips" element={<MyTrips />} />
                   <Route path="todo" element={<TodoList />} />
                   <Route path="favorites" element={<Favorites />} />
